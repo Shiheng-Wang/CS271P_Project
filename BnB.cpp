5581d2bc1dc9a50b51e1261b7797d180f9308ddc
Shiheng-Wang <shihengwang@umail.ucsb.edu>
2020-12-15 13:32:37 -0800

    BnB DFS to be tested

--- BnB.cpp
+++ BnB.cpp
@@ -1,16 +1,59 @@
 #include <vector>
 #include <limits>
+#include <stack>
 #include "Graph.h"
 
 using namespace std;
 
-void BnBHelper(int start, int cur, int UB, vector<bool> visited) {
+float UB, cost;
+stack<int> path, best_path;
 
+bool allVisited(vector<bool> &visited)
+{
+    for (int i = 0; i < visited.size(); i++)
+    {
+        if (!visited[i])
+            return false;
+    }
+    return true;
 }
 
-int BnBDFS(Graph graph) {
-    int UB = numeric_limits<float>::max();
+int h(int node, Graph &graph)
+{
+}
+
+void BnBHelper(int start, int cur, vector<bool>& visited, Graph &graph)
+{
+    if (cur == start && allVisited(visited))
+    {
+        if (cost < UB)
+        {
+            UB = cost;
+            best_path = path;
+        }
+        return;
+    }
+    for (int i = 0; i < graph.get_city_num(); i++)
+    {
+        if (visited[i])
+            continue;
+        if (cost + h(cur, graph) < UB) {
+            path.push(i);
+            visited[i] = true;
+            cost += graph.get_dis(cur, i);
+            BnBHelper(start, i, visited, graph);
+            cost -= graph.get_dis(cur, i);
+            visited[i] = false;
+            path.pop();
+        } 
+    }
+}
+
+int BnBDFS(Graph &graph)
+{
+    UB = numeric_limits<float>::max();
+    cost = 0;
     vector<bool> visited(graph.get_city_num(), false);
-    BnBHelper(0, 0, UB, visited);
+    BnBHelper(0, 0, visited, graph);
     return UB;
 }
